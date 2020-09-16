a27857760bef2f7cf747f6e4810a0a8448613cb8
Liu Weilong <weilong.liu-csu@outlook.com>
2020-09-16 18:07:09 +0800

    add ros-melodic module

---
+++ 9. ros-melodic/src/ros-melodic-test/src/9.1 set-polygen-in-rviz.cpp
@@ -0,0 +1,11 @@
+/*
+ * @Author: Liu Weilong
+ * @Date: 2020-09-16 08:35:59
+ * @LastEditors: Liu Weilong 
+ * @LastEditTime: 2020-09-16 08:52:25
+ * @FilePath: /3rd-test-learning/9. ros-melodic/9.1 set-polygen-in-rviz.cpp
+ * @Description: 用于测试在rviz上面设置多边形的功能
+ */
+
+#include <ros/ros.h>
+#include <>
