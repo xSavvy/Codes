971cb046affe59380d534ecc7ec61217d7c61bc4
Lwl <weilong.liu-csu@outlook.com>
2020-09-18 08:01:44 +0800

    add 8.ceres-Jet-test

---
+++ 8. ceres/5th-how-to-write-function-usable-in-ceres.cpp
@@ -0,0 +1,21 @@
+/*
+ * @Author: Liu Weilong
+ * @Date: 2020-09-18 07:08:43
+ * @LastEditors: Liu Weilong
+ * @LastEditTime: 2020-09-18 07:25:02
+ * @Description: 如何编写一个在ceres中可用的函数
+ */
+
+#include <iostream>
+#include <ceres/ceres.h>
+
+
+
+
+using namespace std;
+ 
+int main(int argc,char **argv)
+{
+    
+    return 0;
+}
