178d5ef24951eda8890895f4267812cad1e812f3
Liu Weilong <weilong.liu-csu@outlook.com>
2020-09-23 18:40:04 +0800

    nothing

---
+++ 11. tic toc/tic_toc-test.cpp
@@ -0,0 +1,26 @@
+/*
+ * @Author: Liu Weilong
+ * @Date: 2020-09-23 16:08:13
+ * @LastEditors: Liu Weilong 
+ * @LastEditTime: 2020-09-23 16:15:36
+ * @FilePath: /3rd-test-learning/11. tic toc/tic_toc-test.cpp
+ * @Description: tic_toc 计时器测试
+ */
+
+// chrono 来自C++11 cmake 需要手动进行设置
+
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    auto start = chrono::high_resolution_clock::now();
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> diff = end - start;
+    auto ms = std::chrono::duration_cast<chrono::microseconds>(diff);
+
+    cout<< ms.count()<<endl;
+    return 0;
+}
---
+++ 2. rosbag-convert/rosbag_info_get.hpp
@@ -0,0 +1,10 @@
+/*
+ * @Author: Liu Weilong
+ * @Date: 2020-09-23 14:07:59
+ * @LastEditors: Liu Weilong 
+ * @LastEditTime: 2020-09-23 14:08:13
+ * @FilePath: /src/m-explore/explore/testing/rosbag_info_get.hpp
+ * @Description: 用于得到rosbag 的info 
+ */
+
+#include "ros"
