de19e9f90c7ea4fc4485fcbb3404b8038c523951
Liu Weilong <weilong.liu-csu@outlook.com>
2020-09-28 16:47:40 +0800

    add ros::spinner part

--- 9. ros-melodic/src/ros-melodic-test/src/9.5.1 multiple-spinner-test.cpp
+++ 9. ros-melodic/src/ros-melodic-test/src/9.5.1 multiple-spinner-test.cpp
@@ -2,40 +2,24 @@
  * @Author: Liu Weilong
  * @Date: 2020-09-25 08:56:51
  * @LastEditors: Liu Weilong 
- * @LastEditTime: 2020-09-25 09:22:13
- * @FilePath: /3rd-test-learning/9. ros-melodic/src/ros-melodic-test/src/9.5 spinner-test.cpp
+ * @LastEditTime: 2020-09-28 16:05:53
+ * @FilePath: /3rd-test-learning/9. ros-melodic/src/ros-melodic-test/src/9.5.1 multiple-spinner-test.cpp
  * @Description: 用于测试ros spin 的性质    
  */
 
 #include <iostream>
 #include <ros/ros.h>
 #include <ros/timer.h>
+#include "ros-melodic-test/test-use-function.hpp"
 using namespace std;
 
-// spin callback 需要添加 ros::TimerEven& 作为参数
-void print(const ros::TimerEvent& event){
-    cout<<"===================================="<<endl;
-
-    for (int i =0;i<30;i++)
-    {
-        cout<<i<<endl;
-    }
-}
-void print1(const ros::TimerEvent& event){
-    cout<<"===================================="<<endl;
-
-    for (int i =30;i<60;i++)
-    {
-        cout<<i<<endl;
-    }
-}
 
 
 
 int main(int argc,char **argv)
 {
     // ros 不允许存在- . 等符号
-    ros::init(argc,argv,"simple_spin_test");
+    ros::init(argc,argv,"spinner_test");
     
     ros::NodeHandle nh;
     auto timer = nh.createTimer(ros::Duration(0.1),print);
