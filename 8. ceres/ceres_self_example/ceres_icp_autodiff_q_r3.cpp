e30376059291568322743f3c67e211fbe780db1e
Lwl <weilong.liu-csu@outlook.com>
2020-11-02 08:05:20 +0800

    添加doc 和doc内部的ceres_in_LOAM

---
+++ 8. ceres/ceres_self_example/ceres_icp_autodiff_q_r3.cpp
@@ -0,0 +1,8 @@
+/*
+ * @Author: Liu Weilong
+ * @Date: 2020-11-02 07:59:53
+ * @LastEditors: Liu Weilong
+ * @LastEditTime: 2020-11-02 08:02:27
+ * @Description: 
+ *                Auto Diff Quternion + R3 上的优化 
+ */
