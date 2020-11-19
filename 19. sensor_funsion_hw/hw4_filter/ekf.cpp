479bf6e2ca1bf3987f211c41637168122b47174b
Lwl <weilong.liu-csu@outlook.com>
2020-11-19 08:17:03 +0800

    ekf

--- 19. sensor_funsion_hw/hw4_filter/ekf.cpp
+++ 19. sensor_funsion_hw/hw4_filter/ekf.cpp
@@ -2,7 +2,7 @@
  * @Author: Liu Weilong
  * @Date: 2020-11-18 07:49:24
  * @LastEditors: Liu Weilong
- * @LastEditTime: 2020-11-19 07:38:37
+ * @LastEditTime: 2020-11-19 08:16:55
  * @Description: 
  */
 #include "ekf.h"
@@ -33,19 +33,3 @@ void EKF::buildProblem(Eigen::Vector3d delta_rotation_measure, Eigen::Vector3d d
 
 
 
-EKFObserError::EKFObserError(Eigen::Matrix<double,6,6> laser_measurement_covar,
-                             Eigen::Vector3d delta_rotation_meas, Eigen::Vector3d delta_translation_meas):
-                             laser_measurement_covar_(laser_measurement_covar),
-                             delta_rotation_(delta_rotation_meas),
-                             delta_translation_(delta_translation_meas)
-{
-    Eigen::LLT<Eigen::Matrix<double,6,6>> LLTSolver(laser_measurement_covar_);
-    Lt_=LLTSolver.matrixL().transpose();
-}
-
-bool EKFObserError::Evaluate(const double * const * params, 
-                             double * residual,
-                             double ** jacobians)const
-{
-    
-}
