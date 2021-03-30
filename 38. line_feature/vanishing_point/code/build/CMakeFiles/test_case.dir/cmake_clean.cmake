bea66fa4e5e35206b0a3307d83eee6246abc3b3e
Lwl <weilong.liu-csu@outlook.com>
2021-03-30 22:57:13 +0800

    daily update

---
+++ 38. line_feature/vanishing_point/code/build/CMakeFiles/test_case.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_case.dir/test_case.cpp.o"
+  "CMakeFiles/test_case.dir/VPDetection.cpp.o"
+  "test_case.pdb"
+  "test_case"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_case.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 38. line_feature/vanishing_point/code/test_case.cpp
+++ 38. line_feature/vanishing_point/code/test_case.cpp
@@ -1,8 +1,8 @@
 /*
  * @Author: Liu Weilong
  * @Date: 2021-03-25 18:53:46
- * @LastEditors: Liu Weilong 
- * @LastEditTime: 2021-03-30 16:12:23
+ * @LastEditors: Liu Weilong
+ * @LastEditTime: 2021-03-30 22:56:40
  * @FilePath: /3rd-test-learning/38. line_feature/vanishing_point/code/test_case.cpp
  * @Description: 
  * 
@@ -11,7 +11,9 @@
  * 1. OpenCVPoseAdjustAndEstimate() 调整bar来调整相机位姿
  * 2. VanishingLineTest()  检测在曼哈顿假设下Vanishing Line 变化和估计差别是不是很大 失败
  * 3. VanishingRansac1Line()
- * 
+ * 4. 2021.3.30 今天添加了重力方面的检测，这个重力方向的测试，也说明了一个问题,就是在 raw pitch 如何变化，raw pitch 是多少 yaw 对应的 VP 就是多少
+ *    yaw的变化，不会对yaw 对应的VP 产生影响
+ *    这也是为什么IMU 的重力加速度可以很好的用于 VPs 的clustering 因为从IMU 重力加速度中得到的yaw pitch + 一个随便的yaw 就可以正确估计出yaw 对应VP的参数
  * 
  * 
  * tips:
@@ -41,34 +43,33 @@ void VPsDetection();
 int main()
 {
     // 基础内容测试
-    se3<<0.,0.,0.,0.,0.,0.;
-    cm.T_w_c = Sophus::SE3d::exp(se3);
+    // se3<<0.,0.,0.,0.,0.,0.;
+    // cm.T_w_c = Sophus::SE3d::exp(se3);
 
-    cout<<"real SO3"<<endl<<cm.T_w_c.so3().matrix().inverse()<<endl;
+    // cout<<"real SO3"<<endl<<cm.T_w_c.so3().matrix().inverse()<<endl;
 
-    bd.vboxobs.push_back(b1.generateObs(&cm));
-    bd.vboxobs.push_back(b2.generateObs(&cm));
-    bd.vboxobs.push_back(b3.generateObs(&cm));
-    bd.vboxobs.push_back(b4.generateObs(&cm));
+    // bd.vboxobs.push_back(b1.generateObs(&cm));
+    // bd.vboxobs.push_back(b2.generateObs(&cm));
+    // bd.vboxobs.push_back(b3.generateObs(&cm));
+    // bd.vboxobs.push_back(b4.generateObs(&cm));
 
-    BoxObservation obs = b1.generateObs(&cm);
-    Eigen::Vector3d vl1 = K_inv*obs.toVL(obs.v2dobs_x,obs.v2dobs_y);
-    vl1.normalize();
-    Camera cm1;
-    Eigen::Matrix<double,6,1> se31;
-    se31<<1.,1.,1.,1.,1.,0.;
-    cm1.T_w_c = Sophus::SE3d::exp(se31);
+    // BoxObservation obs = b1.generateObs(&cm);
+    // Eigen::Vector3d vl1 = K_inv*obs.toVL(obs.v2dobs_x,obs.v2dobs_y);
+    // vl1.normalize();
+    // Camera cm1;
+    // Eigen::Matrix<double,6,1> se31;
+    // se31<<1.,1.,1.,1.,1.,0.;
+    // cm1.T_w_c = Sophus::SE3d::exp(se31);
     
-    BoxObservation obs1 = b1.generateObs(&cm1);
-    Eigen::Vector3d vl2 = K_inv*obs.toVL(obs1.v2dobs_x,obs1.v2dobs_y);
-    vl2.normalize();
-    cout<<"the real vl :"<<vl1.transpose()<<endl;
-    cout<<"the real vl :"<<vl2.transpose()<<endl;
+    // BoxObservation obs1 = b1.generateObs(&cm1);
+    // Eigen::Vector3d vl2 = K_inv*obs.toVL(obs1.v2dobs_x,obs1.v2dobs_y);
+    // vl2.normalize();
+    // cout<<"the real vl :"<<vl1.transpose()<<endl;
+    // cout<<"the real vl :"<<vl2.transpose()<<endl;
+
+    OpenCVPoseAdjustAndEstimate();
+
 }
-
-
-
-
 
 void OpenCVPoseAdjustAndEstimate()
 {
@@ -76,7 +77,7 @@ void OpenCVPoseAdjustAndEstimate()
     cv::createTrackbar("value1：", "ir_gray", &x,300 , Adjust);
     cv::createTrackbar("value2", "ir_gray", &y, 300, Adjust);
     cv::createTrackbar("value3：", "ir_gray", &z, 300, Adjust);
-    cv::createTrackbar("value4", "ir_gray", &r, 300, Adjust);
+    cv::createTrackbar("value4:", "ir_gray", &r, 300, Adjust);
     cv::createTrackbar("value5：", "ir_gray", &p, 300, Adjust);
     cv::createTrackbar("value6", "ir_gray", &yaw, 300, Adjust);
     Adjust(0, 0);
@@ -95,8 +96,17 @@ void Adjust(int, void *)
     double p_ = float(p)/300;
     double yaw_ = float(yaw)/300;
 
-    se3<<x_,y_,z_,r_,p_,yaw_;
-    cm.T_w_c = Sophus::SE3d::exp(se3);
+
+    Eigen::AngleAxisd aa_x(r_,Eigen::Vector3d(1,0,0));
+    Eigen::AngleAxisd aa_y(p_,Eigen::Vector3d(0,1,0));
+    Eigen::AngleAxisd aa_z(yaw_,Eigen::Vector3d(0,0,1));
+
+    cm.T_w_c.setQuaternion((aa_z*aa_y*aa_x));
+
+    cm.T_w_c.translation() = Eigen::Vector3d(x_,y_,z_);
+    
+
+
 
     cout<<"real SO3"<<endl<<cm.T_w_c.so3().matrix().inverse()<<endl;
 
