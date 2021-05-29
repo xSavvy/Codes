29123359677af24fad94eee0aaf39bf9db6ffc84
Lwl <weilong.liu-csu@outlook.com>
2021-05-29 11:08:40 +0800

    ndt_custom

--- 14. pcl/analysis/test_bad_scan.cc
+++ 14. pcl/analysis/test_bad_scan.cc
@@ -2,8 +2,13 @@
  * @Author: Liu Weilong
  * @Date: 2021-05-27 17:09:04
  * @LastEditors: Liu Weilong
- * @LastEditTime: 2021-05-27 19:21:59
+ * @LastEditTime: 2021-05-28 11:12:53
  * @Description: 
+ * 
+ * 主要是用于测试处理之后的点云，是否会出现跳变的问题
+ * +
+ * 尝试权重对整体匹配的影响,真的是人傻了
+ * 
  */
 
 #include <iostream>
@@ -16,7 +21,8 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/filters/voxel_grid_covariance.h>
-#include <pcl/registration/ndt.h>
+
+#include "ndt.h"
 using namespace std;
 
 namespace fs = boost::filesystem;  
@@ -61,7 +67,6 @@ Eigen::Matrix4f v3f2mat4f(const Eigen::Vector3f& t)
 	return res;
 }
 
-
 int main()
 {
     pcl::PointCloud<pcl::PointXYZI>::Ptr map (new pcl::PointCloud<pcl::PointXYZI>());
@@ -79,9 +84,12 @@ int main()
     pcl::PointCloud<pcl::PointXYZI>::Ptr trans_scan (new pcl::PointCloud<pcl::PointXYZI>());
 
     pcl::io::loadPCDFile("/home/lwl/sany_project/sany_agv_slam/lwl_test/200.pcd",*scan);
-    pcl::io::loadPCDFile("/home/lwl/sany_project/sany_agv_slam/lwl_test/200_1.pcd",*scan1);
-    pcl::io::loadPCDFile("/home/lwl/sany_project/sany_agv_slam/lwl_test/200_2.pcd",*scan2);
+    pcl::io::loadPCDFile("/home/lwl/sany_project/sany_agv_slam/lwl_test/200_3.pcd",*scan1); // 修改程度最大的帧
+    pcl::io::loadPCDFile("/home/lwl/sany_project/sany_agv_slam/lwl_test/200_2.pcd",*scan2); // 修改程度稍小的帧
 
+
+    // SimpleShow(scan);
+    SimpleShow(scan1);
     
     Eigen::Vector3f real(-0.649424,5.29211,-1.57293);
     auto real_matrix=v3f2mat4f(real);
@@ -89,7 +97,7 @@ int main()
     pcl::transformPointCloud(*scan,*trans_scan,real_matrix);
     
 
-    pcl::NormalDistributionsTransform<pcl::PointXYZI,pcl::PointXYZI> ndt_;
+    ndt_analysis::NormalDistributionsTransform<pcl::PointXYZI,pcl::PointXYZI> ndt_;
     
     ndt_.setResolution(0.5);
     ndt_.setMaximumIterations(20);
@@ -109,7 +117,7 @@ int main()
 
     * all = * map_color + *scan_color;
     
-    SimpleShow(all);
+    // SimpleShow(all);
 
     ndt_.setInputSource(scan1);
     ndt_.align(*trans_scan,real_matrix);
@@ -126,5 +134,18 @@ int main()
 
     * all = * map_color + *scan_color;
     
-    SimpleShow(all);
+    // SimpleShow(all);
+
+    // 尝试权重对匹配结果的影响
+
+    // pcl::PointCloud<pcl::PointXYZI>::Ptr all_without_color (new pcl::PointCloud<pcl::PointXYZI>());
+    // *all_without_color = *scan+*scan1; 
+    // for(int i = 0;i<2;i++)
+    // {
+    //     *all_without_color += *scan1;
+    // }
+
+    // ndt_.setInputSource(all_without_color);
+    // ndt_.align(*trans_scan,real_matrix);
+    // cout<<"the estimation:"<<ndt_.getFinalTransformation().col(3).transpose()<<endl;
 }
