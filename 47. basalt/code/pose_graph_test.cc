82907ceaa1c17cd08564bffa857f059aa7a3251b
Lwl <weilong.liu-csu@outlook.com>
2021-06-03 22:26:08 +0800

    nothing

--- 47. basalt/code/pose_graph_test.cc
+++ 47. basalt/code/pose_graph_test.cc
@@ -2,18 +2,19 @@
  * @Author: Liu Weilong
  * @Date: 2021-05-11 10:12:58
  * @LastEditors: Liu Weilong
- * @LastEditTime: 2021-05-21 10:56:44
+ * @LastEditTime: 2021-06-03 22:25:38
  * @FilePath: /Codes/47. basalt/code/pose_graph_test.cc
  * @Description: 
  * 
  * 这个文件主要是为了测试 奇怪的位姿图优化
  * 
  * 
- * 具体公式见 doc.md
+ * 具体公式见 doc.md 奇怪的位姿图优化求导 
  * 
  * 没有想到效果和正常版本基本一致
  * 
  * 真的是牛逼
+ * 
  * 
  * 
  * 
